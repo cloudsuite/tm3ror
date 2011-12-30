7b23b26e0079438d1f4bd8704c8b228f8d16cffa
tom atwood <thomas.atwood07@gmail.com>
2011-12-29 22:17:27 -0800

    base_products show accepts params[:id]

--- app/controllers/base_products_controller.rb
+++ app/controllers/base_products_controller.rb
@@ -2,8 +2,8 @@ class BaseProductsController < ApplicationController
    
   def show
      # test by hardwiring a specific base product and its controller
-     @base_product = BaseProduct.where(:tms_product_no => "710060-000").first
-     # or:  @base_product = BaseProduct.where(:id => 3) 
+     # @base_product = BaseProduct.where(:tms_product_no => "710060-000").first
+     @base_product = BaseProduct.where(:id => params[:id]).first 
      @category = @base_product.category
      
      # What url do I use to invoke the show action? 

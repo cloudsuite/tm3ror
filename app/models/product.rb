d6d5da7555c69be6ecc5e9d09be1e40b293a6d05
tom atwood <thomas.atwood07@gmail.com>
2012-01-02 20:52:02 -0800

    added PKs add to cart, checkout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
       belongs_to :product_type
       belongs_to :category
-      belongs_to :accessory_of, :class => "ProductType"
+      # belongs_to :accessory_of, :class => "ProductType"
+
  
 end

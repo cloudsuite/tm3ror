1f03d60b318c04aa36c23090b839f4700263bfef
tom atwood <thomas.atwood07@gmail.com>
2012-05-29 10:49:44 -0700

    Revert "changes to registration"
    This reverts commit c35e44e4a10ab0eb056d4be57952851110d37852.
    
    trying to recover formatting of menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120529000827) do
+ActiveRecord::Schema.define(:version => 20120509221833) do
 
   create_table "accessories", :force => true do |t|
     t.integer  "category_id"
@@ -90,45 +90,6 @@ ActiveRecord::Schema.define(:version => 20120529000827) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-
-  create_table "administrators", :force => true do |t|
-    t.string   "type"
-    t.integer  "tm_customer_id"
-    t.integer  "netsuite_customer_id"
-    t.string   "salutation"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "title"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "phone"
-    t.string   "extension"
-    t.string   "role"
-    t.boolean  "owner"
-    t.boolean  "administrator"
-    t.boolean  "clinician"
-    t.integer  "clinic_id"
-    t.integer  "clinic_tm_id"
-    t.string   "clinic_name"
-    t.string   "address"
-    t.string   "address2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.string   "price_schedule"
-    t.integer  "wants_n_catelogs"
-    t.string   "password"
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "base_products", :force => true do |t|
     t.integer  "category_id"
@@ -348,9 +309,9 @@ ActiveRecord::Schema.define(:version => 20120529000827) do
     t.string   "phone"
     t.string   "extension"
     t.string   "role"
-    t.integer  "owner"
-    t.integer  "administrator"
-    t.integer  "clinician"
+    t.boolean  "owner"
+    t.boolean  "administrator"
+    t.boolean  "clinician"
     t.integer  "clinic_id"
     t.integer  "clinic_tm_id"
     t.string   "clinic_name"
